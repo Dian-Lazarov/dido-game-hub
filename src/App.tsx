074e41fac0d8a937998fa3c3403f15ef8c89a9a8
Dian Lazarov <dian.lazzarov@gmail.com>
2026-06-10 17:59:12 +0300

    Set document title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
@@ -17,6 +17,10 @@ export interface GameQuery {
 }
 
 function App() {
+  useEffect(() => {
+    document.title = "Dido | GameHub";
+  }, []);
+
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
